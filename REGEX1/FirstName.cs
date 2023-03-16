7db53684644476e0167bd07878b2ab1d5ec9416b
shwethab8888 <shwethab8888@gmail.com>
2023-03-16 07:11:17 +0530

    Enter Valid Firstname

---
+++ REGEX1/FirstName.cs
@@ -0,0 +1,41 @@
+﻿using REGEX1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace REGEX1
+{
+    public class FirstName
+    {
+        public static bool IsValid(string firstName)
+        {
+            return Regex.IsMatch(firstName, @"^[A-Z][a-z]{2,}$");
+        }
+
+        public static void First_Name()
+        {
+
+            string firstName;
+
+            while (true)
+            {
+                Console.Write("Enter your first name: ");
+                firstName = Console.ReadLine();
+
+                if (FirstName.IsValid(firstName))
+                {
+                    break;
+                }
+
+                Console.WriteLine("Invalid first name. Please try again.");
+            }
+
+            Console.WriteLine($"Welcome, {firstName}!");
+        }
+
+    }
+
+}
---
+++ REGEX1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace REGEX1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // Console.WriteLine("Welcome to Regex");
+              FirstName.First_Name();
+        
+        }
+    }
+}
